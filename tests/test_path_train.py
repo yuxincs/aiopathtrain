51d00dc4f99d5be4297cacee108e2ae27d428a57
Yuxin Wang <yuxinwang.dev@gmail.com>
2025-12-30 21:38:49 -0500

    Add integration test

---
+++ tests/test_path_train.py
@@ -0,0 +1,14 @@
+from path_train import Direction, PATHRealtimeClient
+
+
+async def test_path_train():
+    client = PATHRealtimeClient()
+
+    station = "Exchange Place"
+    direction: Direction = "New York"
+    async for arrival in client.listen(station, direction):
+        assert arrival.station == station and arrival.direction == direction
+        assert arrival.seconds_to_arrival >= 0
+        assert arrival.line_color
+        assert arrival.headsign
+        break  # A successful delivery of a single message is good enough - we stop here.
